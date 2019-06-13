e6e25519a1e06ecc2fd792c1e28529b7a209e294
Sinan Baltacioglu <siphon@gmail.com>
2019-06-12 21:01:31 -0400

    UT1-J18 Survey Updates

--- static/sur/ut1_june18_event.js
+++ static/sur/ut1_june18_event.js
@@ -1,101 +1,110 @@
-var ut1_june18_event = `# /en Sub Header /; /fr (fr)Sub Header /;
-
+var ut1_june18_event = `# /en UT1-J18 /; /fr UT1-J18 /;
 
 ##
 /en Events Evaluation /;
 /fr (fr)Events Evaluation /; 
 
-
 ###
-/en Description /;
+/en Please let us know about your experience at this event /;
 /fr (fr)Description /; 
 
-
 *Q:
-/en How did you participate in the event? /;
-/fr (fr)How did you participate in the event? /; 
+/en 1. How did you participate in the event? /;
+/fr 1. (fr)How did you participate in the event? /; 
 /ONE
 /en In person /; /fr (fr)In person /; 
 /en Via webcast /; /fr (fr)Via webcast /; 
 ;
 
+*Q: 
+/en 2. Overall, how satisfied were you with this learning event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
+/fr 2. (fr)Overall, how satisfied were you with this learning event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
+/ONE
+1 
+2
+3
+4
+5
+;
 
 *Q: 
-/en Overall, how satisfied were you with this learning event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied? /;
-/fr (fr)Overall, how satisfied were you with this learning event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied? /;
-/scale Very Unsatisfied, Very Satisfied, Unsure
-
-
-*Q: 
-/en Did this event meet your expectations as it was outlined in the advertisement? /;
-/fr (fr)Did this event meet your expectations as it was outlined in the advertisement? /;
+/en 3a. Did this event meet your expectations as it was outlined in the advertisement? /;
+/fr 3a. (fr)Did this event meet your expectations as it was outlined in the advertisement? /;
 /ONE
 /en Yes /; /fr (fr)Yes /; 
 /en No /; /fr (fr)No /; 
 ;
 
-
 Q: 
-/en If no, why not? /;
-/fr (fr)If no, why not? /;
+/en 3b. If no, why not? /;
+/fr 3b. (fr)If no, why not? /;
 /open
 
+/pagebreak
 
 *Q:
-/en How satisfied were you with the speaker(s) for this event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
-/fr (fr)How satisfied were you with the speaker(s) for this event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
-
-/scale Very Unsatisfied, Very Satisfied, Unsure
-
-
-Q: 
-/en Why did you give the score that you did? /;
-/fr (fr)Why did you give the score that you did? /;
-/open
-
-
-*Q: /en Did you learn something new from this event? /;
-/fr (fr)Did you learn something new from this event? /;
+/en 4a. How satisfied were you with the speaker(s) for this event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
+/fr 4a. (fr)How satisfied were you with the speaker(s) for this event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
 /ONE
-/en Yes, I learned a lot of new things /; /fr (fr)Yes, I learned a lot of new things /; 
-/en Yes, I learned something new /; /fr (fr)Yes, I learned something new /; 
-/en No, I did not learn anything new /; /fr (fr)No, I did not learn anything new /; 
+1 
+2
+3
+4
+5
 ;
 
+Q: 
+/en 4b. Why did you give the score that you did? /;
+/fr 4b. (fr)Why did you give the score that you did? /;
+/open
 
 *Q: 
-/en Is there something you can take away from this event that will help you in your job? /;
-/fr (fr)Is there something you can take away from this event that will help you in your job? /;
+/en 5a. Did you learn something new from this event? /;
+/fr 5a. (fr)Did you learn something new from this event? /;
+/ONE
+/en I did not learn anything new /; /fr (fr)I did not learn anything new /;
+/en I learnt something new. /fr (fr)I learnt something new. /;
+/en I learned a lot of new things./fr (fr)I learned a lot of new things. /;
+;
+
+Q: 
+/en 5b. If you learned something during the event, please describe what you have learned. /;
+/fr 5b. (fr)If you learned something during the event, please describe what you have learned. /;
+/open
+
+*Q: 
+/en 6a. Is there something you can take away from this event that will help you in your job? /;
+/fr 6a. (fr)Is there something you can take away from this event that will help you in your job? /;
 /ONE
 /en Yes /; /fr (fr)Yes /; 
 /en No /; /fr (fr)No /; 
 ;
 
-
 Q: 
-/en If yes, what? /;
-/fr (fr)If yes, what? /;
+/en 6b. If yes, what? /;
+/fr 6b. (fr)If yes, what? /;
 /open
 
+/pagebreak
 
 *Q: 
-/en Would you recommend this event to others? /;
-/fr (fr)Would you recommend this event to others? /;
+/en 7a. Would you recommend this event to others? /;
+/fr 7a. (fr)Would you recommend this event to others? /;
 /ONE
 /en Yes /; /fr (fr)Yes /; 
 /en No /; /fr (fr)No /; 
 ;
 
-
 Q: 
-/en Why or why not? /;
-/fr (fr)Why or why not? /;
+/en 7b. Why or why not? /;
+/fr 7b. (fr)Why or why not? /;
 /open
 
-
 Q: 
-/en Please provide any other comments you may have /;
-/fr (fr)Please provide any other comments you may have /;
+/en 8. Other comments /;
+/fr 8. (fr)Other comments /;
 /open
+
+// /en Thank you for your time! /; /fr (fr)Thank you for your time! /;
 
 `;
