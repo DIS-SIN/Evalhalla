ba8c5d827fa7859abb2a12495ee64263163c27da
Sinan Baltacioglu <siphon@gmail.com>
2019-06-21 07:32:21 -0400

    Sample Engage Survey

--- static/sur/engage.js
+++ static/sur/engage.js
@@ -9,10 +9,91 @@ var engage = `# Engage
 /fr Cette évaluation, qui vous prendra de 5 à 10 minutes, nous aidera dans notre processus de conception et nous permettra d’offrir du soutien à la communauté des spécialistes du gouvernement du Canada. Il est possible que nous utilisions les résultats de l’évaluation dans leur ensemble (c’estàdire sans associer les réponses à quiconque) pour aider l’Académie du numérique et les autres partenaires d’apprentissage à planifier et à élaborer des produits d’apprentissage. /;
 
 *Q: 
+/en Overall, how satisfied were you with this learning event? Rate your satisfaction on a scale of 1 to 5, with 1 being very unsatisfied and 5 being very satisfied. /;
+/fr Dans l’ensemble, à quel point êtes-vous satisfait(e) de cette activité d’apprentissage? Évaluez votre satisfaction sur une échelle de 1 à 5, où 1 correspond à « très insatisfait » et 5 correspond à « très satisfait ». /;
+/ONE
+/en 0 - Unsure /; /fr 0 - Incertain /;
+/en 1 - Very Unsatisfied /; /fr  1 - Très insatisfait /;
+/en 2 - Advanced Beginner /; /fr 2 - Débutant avancé /;
+/en 3 - Competent /; /fr 3 - Compétent /;
+/en 4 - Proficient /; /fr 4 - Très compétent /;
+/en 5 - Very Satisfied /; /fr 5 - Très satisfait /;
+;
+
+Q:
+/en Why did you give that score? /;
+/fr Veuillez nous dire les raisons pour lesquelles vous avez attribué cette note à l'activité d'apprentissage. /;
+/OPEN
+
+*Q: 
+/en Did this event meet your expectations as it was outlined in the advertisement? /;
+/fr D’après la description de l’annonce, est-ce que cette activité a répondu à vos attentes? /;
+/ONE
+/en Yes /; /fr Oui /; 
+/en No /; /fr Non /; 
+;
+
+*Q:
+/en Please rate your level of knowledge if the subject area. Before this learning activity: /;
+/fr Veuillez évaluer votre niveau de connaissances dans le domaine en question. Avant l’activité d’apprentissage: /;
+/ONE
+/en High /; /fr Élevé /; 
+/en Good /; /fr Bon /; 
+/en Average /; /fr Moyen /; 
+/en A Little /; /fr Faible /;
+/en None /; /fr Nul /; 
+;
+
+*Q: 
+/en Please rate your level of knowledge if the subject area. After this learning activity:  /;
+/fr Veuillez évaluer votre niveau de connaissances dans le domaine en question. Après l’activité d’apprentissage: /;
+/ONE
+/en High /; /fr Élevé /; 
+/en Good /; /fr Bon /; 
+/en Average /; /fr Moyen /; 
+/en A Little /; /fr Faible /;
+/en None /; /fr Nul /; 
+;
+
+*Q: 
+/en What resources do you use today to learn new things? /; 
+/fr Quelles ressources utilisezvous, actuellement, pour en apprendre? /;
+/any
+/en Podcasts /; /fr Balados /;
+/en Videos /; /fr Vidéos /;
+/en Classroom /; /fr Cours en salle de classe /;
+/en Online courses /; /fr Cours en ligne /;
+/en Peers /; /fr Apprentissage par les pairs /;
+/en Workshops and conferences /; /fr Ateliers et conférencess /;
+;
+
+*Q: 
+/en Would you recommend this event to others? /;
+/fr Recommanderiez-vous cette activité à d’autres personnes? /;
+/ONE
+/en Yes /; /fr Oui /; 
+/en No /; /fr Non /; 
+;
+
+Q: 
+/en Why or why not? /;
+/fr Pourquoi? /;
+/open
+
+*Q: 
 /en Are you a regional employee? /; 
 /fr Travaillezvous en région ? /;
 /ONE
 /en Yes /; /fr Oui /;
 /en No /; /fr Non /;
+;
+
+Q: 
+/en Other comments /;
+/fr Autres commentaires /;
+/open
+
+// /en Thank you for your time! /; /fr Merci pour vos réponses! /;
+
 ;
 `;
