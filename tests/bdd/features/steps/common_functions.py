af2ddd60a75289ea03a24a39e73aa73957901b7d
rukaury <aruka102@uottawa.ca>
2019-06-25 23:05:42 -0400

    Start of survey page steps implementation

--- tests/bdd/features/steps/common_functions.py
+++ tests/bdd/features/steps/common_functions.py
@@ -1,4 +1,5 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By 
 
 def isElementWithTextPresent(context, by, text):
     try:
@@ -13,3 +14,11 @@ def isElementWithIdPresent(context, elem_id):
         return True
     except NoSuchElementException:
         return False
+
+
+def getAllElementsWithPath(context, path):
+    try:
+        elements = context.driver.find_elements(By.XPATH, path)
+        return elements
+    except NoSuchElementException:
+        return None
