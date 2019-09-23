26fab794ee186f9af66d9a24b56e810e88baaa1a
Sinan Baltacioglu <siphon@gmail.com>
2019-09-22 21:16:54 -0400

    Survey Templates ELDP

---
+++ static/sur/EX0405_ADM_ELDP.js
@@ -0,0 +1,289 @@
+var ex0405_adm_edlp_daily = `
+#
+ADM_ELDP_EX0405_DAILY
+
+##
+/en EX04-05 ADM ELDP /;
+/fr EX04-05 SMA PDL /;
+
+###
+/en EX04-05 Assistant Deputy Minister Executive Leadership Development Program /;
+/fr EX04-05 Programme de développement en leadership pour les cadres supérieurs à l'intention des sous-ministres adjoints /;
+
+*Q:
+/en Please select the appropriate day /;  
+/fr Veuillez choisir le jour approprié /;  
+/one
+/en Day 1 (Sep 24) /; /fr Jour 1 (Sep 24) /;
+/en Day 2 (Sep 25) /; /fr Jour 2 (Sep 25) /;
+/en Day 3 (Sep 26) /; /fr Jour 3 (Sep 26) /;
+;
+
+/pagebreak
+
+//
+/en Please rate each of the following statements by clicking the appropriate circle. /;
+/fr Veuillez cliquer sur le cercle qui correspond à votre opinion pour chacun des énoncés ci-dessous. /;
+
+//
+/en Session: <strong style='font-weight:bold;'>TITLE</strong> /;
+/fr Séance : <strong style='font-weight:bold;'>TITRE</strong> /;
+
+*Q:
+/en Please rate your overall satisfaction or dissatisfaction with this session on a scale of 1 to 10, where 1 is 'very dissatisfied' and 10 is 'very satisfied'. /;
+/fr Veuillez indiquer votre degré de satisfaction à l’égard de la séance, sur une échelle de 1 (très insatisfait) à 10 (très satisfait). /;
+/scale
+/en Very dissatisfied /; /fr Très insatisfait /;,
+/en Very satisfied /; /fr Très satisfait /;,
+/en Unsure /; /fr Incertain /;
+
+*Q:
+/en Please rate your satisfaction or dissatisfaction with the quality of the speakers (s) of this learning event on a scale of 1 to 10, where 1 is 'very dissatisfied' and 10 is 'very satisfied'. /;
+/fr Veuillez indiquer votre degré de satisfaction à l’égard des conférenciers, sur une échelle de 1 (très insatisfait) à 10 (très satisfait). /;
+/scale
+/en Very dissatisfied /; /fr Très insatisfait /;,
+/en Very satisfied /; /fr Très satisfait /;,
+/en Unsure /; /fr Incertain /;
+
+/pagebreak
+
+Q:
+/en Please use the space below to provide any comments you may have. /;
+/fr Veuillez utiliser l’espace ci-dessous pour formuler vos commentaires. /;
+/open
+
+`;
+
+var ex0405_adm_edlp_long = `
+#
+ADM_ELDP_EX0405_DAILY
+
+##
+/en EX04-05 ADM ELDP /;
+/fr EX04-05 SMA PDL /;
+
+###
+/en EX04-05 Assistant Deputy Minister Executive Leadership Development Program /;
+/fr EX04-05 Programme de développement en leadership pour les cadres supérieurs à l'intention des sous-ministres adjoints /;
+
+*Q:
+/en Please select the appropriate day /;  
+/fr Veuillez choisir le jour approprié /;  
+/one
+/en Day 1 (Sep 24) /; /fr Jour 1 (Sep 24) /;
+/en Day 2 (Sep 25) /; /fr Jour 2 (Sep 25) /;
+/en Day 3 (Sep 26) /; /fr Jour 3 (Sep 26) /;
+;
+
+*Q:
+/en Your group and level /;
+/fr Quel est votre groupe et niveau /;
+/one
+/en EX-01 /; /fr EX-01 /;
+/en EX-02 /; /fr EX-02 /;
+/en EX-03 /; /fr EX-03 /;
+/en EX-04 /; /fr EX-04 /;
+/en EX-05 /; /fr EX-05 /;
+;
+
+/pagebreak
+
+*Q:
+/en Please rate your overall satisfaction or dissatisfaction with this learning activity on a scale of 1 to 10, where 1 is 'very dissatisfied' and 10 is 'very satisfied'. /;
+/fr Veuillez indiquer votre degré de satisfaction à l’égard de l’activité d’apprentissage, sur une échelle de 1 (très insatisfait) à 10 (très satisfait). /;
+/scale
+/en Very dissatisfied /; /fr Très insatisfait /;,
+/en Very satisfied /; /fr Très satisfait /;,
+/en Unsure /; /fr Incertain /;
+
+Q:
+/en Please tell us why you gave this score. /;
+/fr Veuillez nous dire les raisons pour lesquelles vous avez attribué cette note à l’activité d’apprentissage. /;
+/open
+
+*Q:
+/en Would you say your expectations of this learning activity were exceeded, met, or not met? /;
+/fr Diriez-vous que l’activité d’apprentissage a dépassé vos attentes, répondu à vos attentes ou déçu vos attentes? /;
+/one
+/en Exceeded /; /fr Dépassé mes attentes /;
+/en Met /; /fr Répondu à mes attentes /;
+/en Not met /; /fr Déçu mes attentes /;
+/en Unsure /; /fr Incertain /;
+;
+
+/pagebreak
+
+//
+/en Please rate your satisfaction or dissatisfaction with the following components of this learning event on a scale of 1 to 10, where 1 is 'very dissatisfied' and 10 is 'very satisfied'. /;
+/fr Veuillez indiquer votre degré de satisfaction à l’égard des éléments suivants de l’activité d’apprentissage, sur une échelle de 1 (très insatisfait) à 10 (très satisfait). /;
+
+*Q:
+/en Language quality of the materials /;
+/fr Qualité de la langue du matériel /;
+/scale
+/en Very negative impression /; /fr Très insatisfait /;,
+/en Very positive impression /; /fr Très satisfait /;,
+/en Unsure /; /fr Incertain /;
+
+*Q:
+/en The level of linguistic duality /;
+/fr Le niveau de dualité linguistique /;
+/scale
+/en Very negative impression /; /fr Très insatisfait /;,
+/en Very positive impression /; /fr Très satisfait /;,
+/en Unsure /; /fr Incertain /;
+
+/pagebreak
+
+//
+/en Reflecting on the learning activity, please rate the following statements, where 1 is “a very negative impression” and 10 is “a very positive impression,” in terms of your impression of the experience and potential impact, if any, of the learning activity. /;
+/fr En réfléchissant à l’activité, veuillez évaluer les énoncés suivants sur une échelle de 1 à 10 en fonction de vos impressions sur l’expérience d’apprentissage et sur l’incidence possible de l’activité en question (1 = impression très défavorable, et 10 = impression très favorable). /;
+
+*Q:
+/en This learning activity is a valuable use of my time. /;
+/fr Le temps consacré à l’activité d’apprentissage a été une utilisation judicieuse de mon temps. /;
+/scale
+/en Very negative impression /; /fr Impression très défavorable /;,
+/en Very positive impression /; /fr Impression très favorable /;,
+/en Unsure /; /fr Incertain /;
+
+*Q:
+/en This learning activity is relevant to my development. /;
+/fr L’activité d’apprentissage est pertinente à mon perfectionnement /;
+/scale
+/en Very negative impression /; /fr Impression très défavorable /;,
+/en Very positive impression /; /fr Impression très favorable /;,
+/en Unsure /; /fr Incertain /;
+
+*Q:
+/en I can apply what I have learned  /;
+/fr Je peux mettre en application mes apprentissages /;
+/scale
+/en Very negative impression /; /fr Impression très défavorable /;,
+/en Very positive impression /; /fr Impression très favorable /;,
+/en Unsure /; /fr Incertain /;
+
+/pagebreak
+
+Q:
+/en What have been the most useful elements of this learning activity for you as a leader?  /;
+/fr Quels ont été les éléments les plus utiles de cette activité d’apprentissage pour vous-même en tant que leader? /;
+/open
+
+Q:
+/en Do you have suggestions for improving this learning activity? /;
+/fr Avez-vous des suggestions pour améliorer cette activité d’apprentissage? /;
+/open
+
+Q:
+/en Please provide your department or agency /;
+/fr Veuillez provir votre ministère ou organisme /;
+/department
+
+`;
+
+/*
+
+/en Administrative Tribunals Support Service of Canada /; /fr  Administration du pipe-line du Nord /;
+/en Agriculture and Agri-Food Canada /; /fr  Affaires autochtones et du Nord Canada /;
+/en Atlantic Canada Opportunities Agency /; /fr  Affaires mondiales Canada /;
+/en Canada Border Services Agency /; /fr  Agence canadienne d’évaluation environnementale /;
+/en Canada Council for the Arts /; /fr  Agence canadienne de développement économique du Nord /;
+/en Canada Development Investment Corporation /; /fr  Agence canadienne d'inspection des aliments /;
+/en Canada Economic Development for Quebec Regions /; /fr  Agence de la consommation en matière financière du Canada /;
+/en Canada Revenue Agency /; /fr  Agence de la santé publique du Canada /;
+/en Canada School of Public Service /; /fr  Agence de promotion économique du Canada atlantique /;
+/en Canadian Commercial Corporation /; /fr  Agence des services frontaliers du Canada /;
+/en Canadian Dairy Commission /; /fr  Agence du revenu du Canada /;
+/en Canadian Environmental Assessment Agency /; /fr  Agence fédérale de développement économique pour le Sud de l’Ontario /;
+/en Canadian Food Inspection Agency /; /fr  Agence spatiale canadienne /;
+/en Canadian Grain Commission /; /fr  Agriculture et Agroalimentaire Canada /;
+/en Canadian Heritage /; /fr  Anciens Combattants Canada /;
+/en Canadian Human Rights Commission /; /fr  Bibliothèque et Archives Canada /;
+/en Canadian Institutes of Health Research /; /fr  Bureau de l’enquêteur correctionnel /;
+/en Canadian Intergovernmental Conference Secretariat /; /fr  Bureau de la sécurité des transports du Canada /;
+/en Canadian Museum of Nature /; /fr  Bureau du Conseil privé /;
+/en Canadian Northern Economic Development Agency /; /fr  Bureau du directeur parlementaire du budget /;
+/en Canadian Nuclear Safety Commission /; /fr  Bureau du secrétaire du gouverneur général /;
+/en Canadian Radio-television and Telecommunications Commission /; /fr  Bureau du surintendant des institutions financières /;
+/en Canadian Space Agency /; /fr  Bureau du vérificateur général /;
+/en Canadian Transportation Agency /; /fr  Centre d’analyse des opérations et déclarations financières du Canada /;
+/en Civilian Review and Complaints Commission for the RCMP /; /fr  Centre de la sécurité des télécommunications Canada /;
+/en Communications Security Establishment Canada /; /fr  Comité de surveillance des activités de renseignement de sécurité /;
+/en Copyright Board Canada /; /fr  Comité des parlementaires sur la sécurité nationale et le renseignement /;
+/en Correctional Service of Canada /; /fr  Comité externe d’examen de la Gendarmerie royale du Canada /;
+/en Courts Administration Service /; /fr  Comité externe d’examen des griefs militaires /;
+/en Department of Finance Canada /; /fr  Commissariat à l’intégrité du secteur public du Canada /;
+/en Elections Canada /; /fr  Commissariat à la magistrature fédérale Canada /;
+/en Employment and Social Development Canada /; /fr  Commissariat à la protection de la vie privée du Canada /;
+/en Environment and Climate Change Canada /; /fr  Commissariat à l'information du Canada /;
+/en Farm Products Council of Canada /; /fr  Commissariat au lobbying du Canada /;
+/en Federal Bridge Corporation Limited /; /fr  Commissariat aux conflits d'intérêts et à l'éthique /;
+/en Federal Economic Development Agency for Southern Ontario /; /fr  Commissariat aux langues officielles /;
+/en Financial Consumer Agency of Canada /; /fr  Commission canadienne de sûreté nucléaire /;
+/en Financial Transactions and Reports Analysis Centre of Canada /; /fr  Commission canadienne des droits de la personne /;
+/en Fisheries and Oceans Canada /; /fr  Commission canadienne des grains /;
+/en Global Affairs Canada /; /fr  Commission canadienne du lait /;
+/en Health Canada /; /fr  Commission civile d’examen et de traitement des plaintes relatives à la GRC /;
+/en Immigration and Refugee Board of Canada /; /fr  Commission d’examen des plaintes concernant la police militaire /;
+/en Immigration, Refugees and Citizenship Canada /; /fr  Commission de l’immigration et du statut de réfugié du Canada /;
+/en Indian Oil and Gas Canada /; /fr  Commission de la capitale nationale /;
+/en Indigenous and Northern Affairs Canada /; /fr  Commission de la fonction publique du Canada /;
+/en Infrastructure Canada /; /fr  Commission des libérations conditionnelles du Canada /;
+/en Innovation, Science and Economic Development Canada /; /fr  Commission du droit d’auteur Canada /;
+/en International Joint Commission /; /fr  Commission mixte internationale /;
+/en Justice Canada /; /fr  Condition féminine Canada /;
+/en Library and Archives Canada /; /fr  Conseil d’examen du prix des médicaments brevetés Canada /;
+/en Military Grievances External Review Committee /; /fr  Conseil de la radiodiffusion et des télécommunications /;
+/en Military Police Complaints Commission /; /fr  Conseil de recherches en sciences humaines du Canada /;
+/en National Capital Commission /; /fr  Conseil de recherches en sciences naturelles et en génie du Canada /;
+/en National Defence - military personnel (uniformed) /; /fr  Conseil des arts du Canada /;
+/en National Defence (civilian) /; /fr  Conseil des produits agricoles du Canada /;
+/en National Energy Board /; /fr  Conseil national de recherches du Canada /;
+/en National Film Board /; /fr  Corporation commerciale canadienne /;
+/en National Research Council Canada /; /fr  Cour suprême du Canada /;
+/en Natural Resources Canada /; /fr  Défense nationale - personnel militaire (en uniforme) /;
+/en Natural Sciences and Engineering Research Council of Canada /; /fr  Défense nationale (civile) /;
+/en Northern Pipeline Agency /; /fr  Développement économique Canada pour les régions du Québec /;
+/en Office of Auditor General /; /fr  Diversification de l’économie de l’Ouest Canada /;
+/en Office of the Commissioner for Federal Judicial Affairs Canada /; /fr  École de la fonction publique du Canada /;
+/en Office of the Commissioner of Lobbying of Canada /; /fr  Élections Canada /;
+/en Office of the Commissioner of Official Languages /; /fr  Emploi et Développement social Canada /;
+/en Office of the Conflict of Interest and Ethics Commissioner /; /fr  Environnement et Changement climatique Canada /;
+/en Office of the Correctional Investigator Canada /; /fr  Gendarmerie royale du Canada - membres en uniforme et membres civils (avec numéro RCM) /;
+/en Office of the Information Commissionner /; /fr  Gendarmerie royale du Canada (employés civils) /;
+/en Office of the Parliamentary Budget Officer /; /fr  Immigration, Réfugiés et Citoyenneté Canada /;
+/en Office of the Privacy Commissionner /; /fr  Infrastructure Canada /;
+/en Office of the Public Sector Integrity Commissioner of Canada /; /fr  Innovation, Sciences et Développement économique Canada /;
+/en Office of the Secretary to the Governor General /; /fr  Instituts de recherche en santé du Canada /;
+/en Office of the Superintendent of Financial Institutions /; /fr  Justice Canada /;
+/en Parks Canada /; /fr  La Corporation de développement des investissements du Canada /;
+/en Parole Board of Canada /; /fr  La société des ponts fédéraux Limitée /;
+/en Patented Medicine Prices Review Board Canada /; /fr  Ministère des Finances Canada /;
+/en Polar Knowledge Canada /; /fr  Musée canadien de la nature /;
+/en Privy Council Office /; /fr  Office des transports du Canada /;
+/en Public Health Agency of Canada /; /fr  Office national de l’énergie /;
+/en Public Prosecution Service of Canada /; /fr  Office national du film du Canada /;
+/en Public Safety Canada /; /fr  Opérations des enquêtes statistiques /;
+/en Public Service Commission of Canada /; /fr  Parcs Canada /;
+/en Public Services and Procurement Canada /; /fr  Patrimoine canadien /;
+/en Royal Canadian Mounted Police - uniformed members and civilian members (with RCM number) /; /fr  Pêches et Océans Canada /;
+/en Royal Canadian Mounted Police (civilian) /; /fr  Pétrole et gaz des Indiens Canada /;
+/en Royal Canadian Mounted Police External Review Committee /; /fr  Ressources naturelles Canada /;
+/en Secretariat of the National Security and Intelligence Committee of Parliamentarians /; /fr  Santé Canada /;
+/en Security Intelligence Review Committee /; /fr  Savoir Polaire Canada /;
+/en Shared Services Canada /; /fr  Secrétariat des conférences intergouvernementales /;
+/en Social Sciences and Humanities Research Council of Canada /; /fr  Secrétariat du Conseil du Trésor du Canada /;
+/en Statistical Survey Operations /; /fr  Sécurité publique Canada /;
+/en Statistics Canada /; /fr  Service administratif des tribunaux judiciaires /;
+/en Status of Women Canada /; /fr  Service canadien d'appui aux tribunaux administratifs /;
+/en Supreme Court of Canada /; /fr  Service correctionnel du Canada /;
+/en Transport Canada /; /fr  Service des poursuites pénales du Canada /;
+/en Transportation Safety Board of Canada /; /fr  Services partagés Canada /;
+/en Treasury Board of Canada Secretariat /; /fr  Services publics et Approvisionnement Canada /;
+/en Veterans Affairs Canada /; /fr  Statistique Canada /;
+/en Veterans Review and Appeal Board /; /fr  Transports Canada /;
+/en Western Economic Diversification Canada /; /fr  Tribunal des Anciens combattants [révision et appel] /;
+/en Other /; /fr  Autre /;
+
+*/
