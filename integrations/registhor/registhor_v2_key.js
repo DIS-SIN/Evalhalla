d873d2aa6e135f7bd5298e0a3c02c0736f485b33
Sinan Baltacioglu <siphon@gmail.com>
2019-10-09 18:09:17 -0400

    Registhor new routes

--- integrations/registhor/registhor_v2_key.js
+++ integrations/registhor/registhor_v2_key.js
@@ -1,5 +1,17 @@
 // registhor connection
 const api_key = "aglutenbergmygoodsir";
+// post route is deprecated since June mvp. Refactor out in next version.
+const api_post_surv_resp_route = "https://registhor.da-an.ca/api/v1/evalhalla/post?key=";
+const api_get_offering_route = "https://registhor.da-an.ca/api/v1/offerings/offering-information?date_1=%currentdate&key=";
+const api_get_classifications_route = "https://registhor.da-an.ca/api/v1/evalhalla/classifications?key=";
+const api_get_departments_route = "https://registhor.da-an.ca/api/v1/evalhalla/departments?key=";
+const api_get_departments_route_fr = "https://registhor.da-an.ca/api/v1/evalhalla/departments?lang=fr&key=";
+const api_get_cities_route = "https://registhor.da-an.ca/api/v1/evalhalla/cities?key=";
+const api_get_cities_route_fr = "https://registhor.da-an.ca/api/v1/evalhalla/cities?lang=fr&key=";
+
+/*
+// registhor connection previous implementation
+const api_key = "aglutenbergmygoodsir";
 const api_post_surv_resp_route = "https://registhor.da-an.ca/api/v1/evalhalla/post?key=";
 const api_get_offering_route = "https://registhor.da-an.ca/api/v1/offerings?date_1=%currentdate&key=";
 const api_get_classifications_route = "https://registhor.da-an.ca/api/v1/registrations/classifications?key=";
@@ -7,6 +19,5 @@ const api_get_departments_route = "https://registhor.da-an.ca/api/v1/registratio
 const api_get_departments_route_fr = "https://registhor.da-an.ca/api/v1/registrations/departments?lang=fr&key=";
 const api_get_cities_route = "https://registhor.da-an.ca/api/v1/registrations/cities?key=";
 const api_get_cities_route_fr = "https://registhor.da-an.ca/api/v1/registrations/cities?lang=fr&key=";
-
-
+*/
 
